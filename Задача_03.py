82bf78f60e58f304284ad031f8fa90e0e12aa8d4
Hohnerd <kaloshin1977@yandex.ru>
2022-05-29 20:40:41 +0500

    Added task

---
+++ Задача_03.py
@@ -0,0 +1,17 @@
+# Составить список простых множителей натурального числа N
+
+n = int(input('Введите натуральное число: '))
+while n > 1:
+    i = 2
+    f = 0
+    while 1:
+        if n%i == 0:
+            n = n // i
+            print(i, end=' ')
+            f = 1
+            break
+        else:
+            i += 1
+    if f == 1:
+        continue
+print()
