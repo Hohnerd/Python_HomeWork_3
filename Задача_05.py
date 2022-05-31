773e6b1ecc73fae6d4b34919ae7a1d50b55d26a9
Hohnerd <kaloshin1977@yandex.ru>
2022-05-31 22:21:31 +0500

    Finish task

--- Задача_05.py
+++ Задача_05.py
@@ -1,13 +1,27 @@
 # Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа. 
 
+
 # 1. Создаю текстовый файл с целыми числами
 with open('Numbers.txt','w') as data:
     data.write('1 2 3 4 5 9 7 3 2 6 8 34 54 11 99 57 92\n') 
-
+print('')
 # 2. Читаю текстовый файл
+print('Изначальный текстовый файл: ')    
 path = 'Numbers.txt'
 data = open(path,'r')
 for line in data:
-    print(line)
-data.close()          
-    
+    print(line)   
+data.close()  
+print('Текстовый файл без чётных чисел: ')
+# Преобразую файл содержащий строки с список, и иду циклом for в обратной последовательности 
+# чтобы урать чётные числа
+m = line.split()
+newList = list(map(int,m))  
+for i in reversed(newList):
+    if i%2==0: newList.remove(i)         
+print(newList)
+# дописываю в исходный файл числа за исключением чётных
+with open('Numbers.txt','a') as data:
+    data.write(str(newList)) 
+data.close()    
+
